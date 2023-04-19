d32dc4fba87481295d99096c84fa1800f4b4fdd9
ashish0121 <ashish0121@users.noreply.github.com>
2023-04-20 01:13:05 +0530

    commenting the gradio code
    moving all gradio code under comment block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import numpy as np 
 import streamlit as st 
-import gradio as gr
-import requests
+#import gradio as gr
+#import requests
 
 st.title('Welcome to Cohort 11 Image Restoration Project!')
 
@@ -35,30 +35,30 @@ if st.button('Restore'):
 else:
     st.write('Goodbye')
 
-def image_restored(file_obj):
-    #here you can plug you AI model. This will enable the code to run
-    # the output will be path to your image that will be hosted on server
-    #once done pass that path in below url param
-    # i have used dummy path from a server to show the output
-    url = 'https://publish.purewow.net/wp-content/uploads/sites/2/2021/06/smallest-dog-breeds-toy-poodle.jpg?fit=728%2C524'
-    img = open('dog.jpg', 'wb')
-    img.write(requests.get(url).content)
-    img.close()
-    return 'dog.jpg'
+#def image_restored(file_obj):
+#    #here you can plug you AI model. This will enable the code to run
+#    # the output will be path to your image that will be hosted on server
+#    #once done pass that path in below url param
+#    # i have used dummy path from a server to show the output
+#    url = 'https://publish.purewow.net/wp-content/uploads/sites/2/2021/06/smallest-dog-breeds-toy-poodle.jpg?fit=728%2C524'
+#    img = open('dog.jpg', 'wb')
+#    img.write(requests.get(url).content)
+#    img.close()
+#    return 'dog.jpg'
 
 #examples = [
 #    [os.path.abspath("short-pdf.pdf")],
 #    [os.path.abspath("long-pdf.pdf")]
 #]
 
-iface = gr.Interface(fn = image_restored, 
-                     inputs = 'image', 
-                     outputs = 'image', 
-                     title = 'Blurred image to restored image Application',
-                     description = 'Simple application in python to try the Gradio package components',
-                     article = 
-                        '''<div>
-                            <p> Please wait while restoration of your image is happening.</p>
-                        </div>'''
-                    )
+#iface = gr.Interface(fn = image_restored, 
+#                     inputs = 'image', 
+#                     outputs = 'image', 
+#                     title = 'Blurred image to restored image Application',
+#                     description = 'Simple application in python to try the Gradio package components',
+#                     article = 
+#                        '''<div>
+#                            <p> Please wait while restoration of your image is happening.</p>
+#                        </div>'''
+#                    )
 #iface.launch()
